178ff4971c1ae1d37b8e4bd628b066af43322e56
Merih <merimemax@gmail.com>
2019-08-25 09:26:14 -0400

    updates

--- eventgrad/src/main/java/com/GradEvent/gradEvent/model/Event.java
+++ eventgrad/src/main/java/com/GradEvent/gradEvent/model/Event.java
@@ -1,11 +1,14 @@
 package com.GradEvent.gradEvent.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
-public class Event {
+public class Event implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,9 +21,15 @@ public class Event {
     private String eventAddress;
 
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade =  CascadeType.ALL, mappedBy = "events")
-    private List<Person> participant = new ArrayList<>();
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "events")
+//    @Fetch(FetchMode.SELECT)
+//    @JsonManagedReference // we use this if it is not collections framework
+//    @JsonBackReference // we use this for collections framework
+    private Set<Person> participant;
 
+    public Event() {
+        this.participant = new HashSet<>();
+    }
 
     public long getEvent_id() {
         return event_id;
@@ -62,11 +71,33 @@ public class Event {
         this.eventAddress = eventAddress;
     }
 
-    public List<Person> getParticipant() {
+    public Set<Person> getParticipant() {
         return participant;
     }
 
-    public void setParticipant(List<Person> participant) {
+    public void setParticipant(Set<Person> participant) {
         this.participant = participant;
+    }
+
+    public void addParticipant(Person person) {
+        this.participant.add(person);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return event_id == event.event_id &&
+                Objects.equals(eventName, event.eventName) &&
+                Objects.equals(eventDescription, event.eventDescription) &&
+                Objects.equals(eventDate, event.eventDate) &&
+                Objects.equals(eventAddress, event.eventAddress);
+//                Objects.equals(participant, event.participant);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(event_id, eventName, eventDescription, eventDate, eventAddress); // participant removed to avoid infinite loop
     }
 }
--- eventgrad/src/main/java/com/GradEvent/gradEvent/model/Person.java
+++ eventgrad/src/main/java/com/GradEvent/gradEvent/model/Person.java
@@ -1,32 +1,88 @@
 package com.GradEvent.gradEvent.model;
 
-import com.GradEvent.gradEvent.service.EventService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
-public class Person {
+public class Person implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "person_id")
     private long person_id;
     private String name;
-
-    @ManyToMany(cascade = CascadeType.ALL)
+    @Column(unique = true)
+    private String email;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
             name = "Person_Event",
             joinColumns = {@JoinColumn(name = "person_id")},
             inverseJoinColumns = {@JoinColumn(name = "event_id")}
     )
-    private Event events;
+//    @Fetch(FetchMode.SELECT)
+    @JsonBackReference  // we use this for collections framework
+//    @JsonManagedReference // we use this if it is not collections
+    private Set<Event> events;
 
+    public Person() {
+        this.events = new HashSet<>();
+    }
 
+    public long getPerson_id() {
+        return person_id;
+    }
 
+    public void setPerson_id(long person_id) {
+        this.person_id = person_id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void addEvent(Event event) {
+        this.events.add(event);
+        event.addParticipant(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return person_id == person.person_id &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(email, person.email) ;
+//                Objects.equals(events, person.events); //we don't need to compare the events to compare person
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(person_id, name, email); // events attribute is removed because we don't need to compare events in person plus it will create recursive infinite loop when we are using many to many relationships
+    }
 }
